2cce12389c273b1d0e872f5a8322027b57e51fec
Mohsen Ghalem <ghalemmohsen@gmail.com>
2023-07-19 23:40:58 +0100

    Initial commit

---
+++ election_system_admin/android/app/src/main/kotlin/com/example/election_system_admin/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.election_system_admin
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ election_system_user/android/app/src/main/kotlin/com/example/election_system_user/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.election_system_user
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
